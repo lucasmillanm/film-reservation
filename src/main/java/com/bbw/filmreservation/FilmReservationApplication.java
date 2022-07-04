e440eba8e50933e1699a82bf694b61d9a64f8b2c
lucasmillanm <70648632+lucasmillanm@users.noreply.github.com>
2022-07-04 09:08:25 +0200

    Initial commit

---
+++ src/main/java/com/bbw/filmreservation/FilmReservationApplication.java
@@ -0,0 +1,13 @@
+package com.bbw.filmreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmReservationApplication.class, args);
+	}
+
+}
