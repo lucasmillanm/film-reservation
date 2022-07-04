b8c46516e481050301ea02132752912353055102
lucasmillanm <70648632+lucasmillanm@users.noreply.github.com>
2022-07-04 15:55:11 +0200

    update files

---
+++ src/main/java/com/bbw/filmreservation/Film.java
@@ -0,0 +1,13 @@
+package com.bbw.filmreservation;
+
+public class Film {
+    private String filmName;
+    private String length;
+    private String language;
+
+    public Film(String filmName, String length, String language) {
+        this.filmName = filmName;
+        this.length = length;
+        this.language = language;
+    }
+}
--- src/main/java/com/bbw/filmreservation/FilmReservationModel.java
+++ src/main/java/com/bbw/filmreservation/FilmReservationModel.java
@@ -2,6 +2,9 @@ package com.bbw.filmreservation;
 
 import jakarta.validation.constraints.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FilmReservationModel {
     @NotNull
     @Size(min = 1, max = 16)
@@ -22,5 +25,62 @@ public class FilmReservationModel {
     @Min(12)
     private int age;
 
-    private boolean withExtras;
+    @NotNull
+    private List<Film> movieList = new ArrayList<>();
+
+    public FilmReservationModel() {
+        movieList.add(new Film("Batman", "200 min", "English"));
+        movieList.add(new Film("Minions", "150 min", "English"));
+        movieList.add(new Film("Maze Runner", "122 min", "English"));
+        movieList.add(new Film("The Northman", "137 min", "English"));
+        movieList.add(new Film("Top Gun: Maverick", "133 min", "English"));
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNr() {
+        return phoneNr;
+    }
+
+    public void setPhoneNr(String phoneNr) {
+        this.phoneNr = phoneNr;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<Film> getMovieList() {
+        return movieList;
+    }
+
+    public void setMovieList(List<Film> movieList) {
+        this.movieList = movieList;
+    }
 }
